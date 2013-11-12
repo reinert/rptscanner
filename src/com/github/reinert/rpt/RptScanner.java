c622e73637f9d05a65f12284267cf4f700173159
Danilo Reinert <daniloreinert@gmail.com>
2013-11-12 19:17:31 -0300

    added more system properties support

--- src/com/github/reinert/rpt/RptScanner.java
+++ src/com/github/reinert/rpt/RptScanner.java
@@ -20,12 +20,29 @@ public class RptScanner {
     public static void main(String[] args) throws Exception {
 
         if (args.length == 1) {
-            throw new Exception("O caminho do arquivo rpt deve ser informado como primeiro argumento. Em seguida, para cada coluna do rpt deve ser informado 0 se a coluna for numero e 1 se for texto. Por fim, opcionalmente podem ser informados os charsets de entrada e saida com as variaveis de contexto 'charset.in' e 'charset.out' e o caracter delimitador de colunas com a varaivel 'delimiter'.");
+            throw new Exception("O caminho do arquivo rpt deve ser informado como primeiro argumento. Em seguida, para cada coluna do rpt deve ser informado 0 se a coluna for numero, 1 se for texto e 2 se for boleano. Por fim, opcionalmente podem ser informados os charsets de entrada e saida com as variaveis de contexto 'charset.in' e 'charset.out' e o caracter delimitador de colunas com a varaivel 'delimiter'. Tambem e possivel informar as strings de entrada para true (boolean.true) e false (boolean.false) no caso dos campos boleanos");
         }
 
         String inCharset = System.getProperty("charset.in", "UTF-8");
         String outCharset = System.getProperty("charset.out", "UTF-8");
         String outDelimiter = System.getProperty("delimiter", ",");
+        String inTrue = System.getProperty("true.in", "true");
+        String inFalse = System.getProperty("false.in", "false");
+        String outTrue = System.getProperty("true.out", "true");
+        String outFalse = System.getProperty("false.out", "false");
+        String[] nullStrings = System.getProperty("null.in", "NULL").split(",");
+        String outNull = System.getProperty("null.out", "NULL");
+
+        String notNullColsParam = System.getProperty("notnull.cols");
+        int[] notnullCols = null;
+        if (notNullColsParam != null) {
+            String[] notnullColsStr = notNullColsParam.split(",");
+            notnullCols = new int[notnullColsStr.length];
+            for (int i = 0; i < notnullColsStr.length; i++) {
+                String notnullCol = notnullColsStr[i];
+                notnullCols[i] = Integer.parseInt(notnullCol);
+            }
+        }
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(args[0]), inCharset));
 
@@ -35,7 +52,7 @@ public class RptScanner {
 
         if ((limitsArray.length + 1) != (args.length)) {
             throw new Exception("O rpt contem " + limitsArray.length + " colunas e vc informou " + (args.length - 1)
-                    + ". Para cada coluna no arquivo rpt vc deve informar 0 (numero) ou 1 (texto).");
+                    + ". Para cada coluna no arquivo rpt vc deve informar 0 (numero), 1 (texto), ou 2 (boleano).");
         }
 
         int[][] limits = new int[limitsArray.length][3];
@@ -72,21 +89,20 @@ public class RptScanner {
 
             delimiter = "";
 
+            // Trata todas as colunas exceto a ultima
             for (int i = 0; i < limits.length-1; i++) {
                 int[] limit = limits[i];
                 stringBuilder.append(delimiter);
                 final String value = line.substring(limit[0], limit[1]).trim();
-                if (limit[2] == 1) {
-                    stringBuilder.append(aspas);
-                    stringBuilder.append(StringEscapeUtils.escapeCsv(value));
-                    stringBuilder.append(aspas);
-                } else {
-                    stringBuilder.append(StringEscapeUtils.escapeCsv(value));
-                }
+                appendValue(outNull ,nullStrings, notnullCols, inTrue, inFalse, outTrue, outFalse, stringBuilder, aspas, i, limit[2], value);
                 delimiter = outDelimiter;
             }
+
+            // Trata a ultima coluna
             stringBuilder.append(delimiter);
-            stringBuilder.append(line.substring(limits[limits.length-1][0]).trim());
+            final int[] limit = limits[limits.length - 1];
+            appendValue(outNull ,nullStrings, notnullCols, inTrue, inFalse, outTrue, outFalse, stringBuilder, aspas, limits.length - 1, limit[2],
+                    line.substring(limit[0]).trim());
 
             stringBuilder.append(LINE_BREAK);
         }
@@ -99,6 +115,49 @@ public class RptScanner {
         }
         finally {
             out.close();
+        }
+    }
+
+    private static void appendValue(String outNull, String[] nullStrings, int[] notNullCols, String inTrue, String inFalse,
+                                    String outTrue, String outFalse, StringBuilder stringBuilder, String aspas,
+                                    int col, int colType, String value) throws Exception {
+
+        boolean isNull = false;
+        boolean isNotNullCol = false;
+        if (notNullCols != null) {
+            for (int i = 0; i < notNullCols.length; i++) {
+                if (col == notNullCols[i]) {
+                    isNotNullCol = true;
+                    break;
+                }
+            }
+        }
+        if (!isNotNullCol) {
+            for (int i = 0; i < nullStrings.length; i++) {
+                if (nullStrings[i].equalsIgnoreCase(value)) {
+                    isNull = true;
+                    break;
+                }
+            }
+        }
+        if (isNull) {
+            stringBuilder.append(outNull);
+        } else if (colType == 1) {
+            String str = StringEscapeUtils.escapeCsv(value);
+            stringBuilder.append(aspas);
+            if (str.startsWith("\"") && str.endsWith("\"")) str = str.substring(1, str.length()-1);
+            stringBuilder.append(str);
+            stringBuilder.append(aspas);
+        } else if (colType == 2) {
+            if (inTrue.equals(value)) {
+                stringBuilder.append(outTrue);
+            } else if (inFalse.equals(inFalse)) {
+                stringBuilder.append(outFalse);
+            } else {
+                throw new Exception("Nao foi possivel ler as entradas da coluna " + col + " (boleana). Configure os valores de entrada boleanos atraves das variaveis de contexto 'boolean.true' e 'boolean.false'.");
+            }
+        } else {
+            stringBuilder.append(StringEscapeUtils.escapeCsv(value));
         }
     }
 }
